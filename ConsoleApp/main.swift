b4c5aa648fd134fbea97f18df106024a4bafac8d
Rob DeCarlo <rdecarlo73@Robs-MacBook-Pro.local>
2017-05-13 11:00:11 -0400

    Initial Commit

---
+++ ConsoleApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConsoleApp
+//
+//  Created by Rob DeCarlo on 5/13/17.
+//  Copyright © 2017 Rob DeCarlo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
